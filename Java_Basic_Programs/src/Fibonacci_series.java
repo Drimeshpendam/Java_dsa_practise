6a9255d5a7fe06fedf2bc4e692a014d1992340f3
Drimesh <69688465+Drimeshpendam@users.noreply.github.com>
2023-09-16 23:48:19 +0530

    Updated File

---
+++ Java_Basic_Programs/src/Fibonacci_series.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/*
+Fibonacci_series
+
+0 1 2 3 5 8 13 . . . . . n;
+
+
+
+ */
+public class Fibonacci_series
+{
+
+    public static void main(String[] args) {
+        int sum=0;
+        System.out.println("Enter Number to Start Series From : -" );
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("You have entered No : -  " +a);
+        for(int j = 0 ;j <= a ; j++)
+        {
+
+        }
+
+        System.out.println();
+
+    }
+}
+
